17e3d0cb39f14c5ff042234860cf8e6e84c5832f
nghia64582 <nghia64582@gmail.com>
2025-07-13 01:49:56 +0700

    Update insert_tables.sql

--- insert_tables.sql
+++ insert_tables.sql
@@ -1,5 +1,6 @@
 CREATE TABLE key_value_store (
     id INT AUTO_INCREMENT PRIMARY KEY,
     `key` VARCHAR(255) UNIQUE NOT NULL,
-    `value` TEXT
+    `value` TEXT,
+    `created_at` TIMESTAMP default CURRENT_TIMESTAMP
 );
